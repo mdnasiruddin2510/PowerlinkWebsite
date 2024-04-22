65a487d1fd9778625a089a987d708a3ab4d821b3
Biplob-Hossain1992 <biplobcse.diu@gmail.com>
2024-04-23 00:19:18 +0600

    working on shoping cart

---
+++ Website/Controllers/CartController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PosWebsite.Controllers
+{
+    public class CartController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
