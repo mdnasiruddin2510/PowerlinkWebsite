4a03eefe6c02b522b5043528010f8ab006adb834
Biplob-Hossain1992 <biplobcse.diu@gmail.com>
2024-04-28 22:00:56 +0600

    lot of changes

---
+++ Website/Controllers/CustomerController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Website.Controllers
+{
+    public class CustomerController : Controller
+    {
+        public IActionResult AccountDetails()
+        {
+            return View();
+        }
+        public IActionResult BillingAddress()
+        {
+            return View();
+        }
+        public IActionResult Wishlist()
+        {
+            return View();
+        }
+        public IActionResult OrderHistory()
+        {
+            return View();
+        }
+        public IActionResult TrackingOrder()
+        {
+            return View();
+        }
+    }
+}
